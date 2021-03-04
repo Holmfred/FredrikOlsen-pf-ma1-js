34599f5b94c63569c5cb0ef8329795d6413f38f3
Holmfred <fredrik1304@live.no>
2021-03-04 17:24:49 +0100

    Done

---
+++ Scripts/scripts.js
@@ -0,0 +1,31 @@
+//Question 1 
+var numberValue = 3;
+var booleanValue = true;
+var stringValue = "Yes";
+
+//Question 2
+var firstName = Fredrik;
+var lastName = Olsen;
+var fullName = firstName + "-" + lastName;
+
+console.log(fullName);
+
+//Question 3
+var animal = "frog";
+var typeOfAnimal = typeOf(animal);
+
+console.log("The animal is",typeOfAnimal)
+
+//Question 4
+var orderHasShipped = true
+
+if (orderHasShipped === true){
+    console.log("The order shipped")
+}else {
+    console.log ("The order did not ship")
+}
+
+//Question 5
+for (var i = 7; i <= 13; i++) {
+    console.log(i)
+}
